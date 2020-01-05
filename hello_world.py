f78f9739d8869f9ccbab29a695050f4a63ea2092
Peter Norton <peter.norton@gazprom-mt.com>
2020-01-05 21:35:59 +0000

    hello world

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from pprint import pprint as pp
+from pytube import YouTube
+
+print(os.getcwd())
+pp(sys.path)
+
+url = r'https://www.youtube.com/watch?v=xZAfdgYnsZc&list=PLYf3-nc0C5ly-2-7WU6S2nCW7MMT0VM87&index=2'
+yt = YouTube(url)
+print(yt.description)
