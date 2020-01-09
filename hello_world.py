a18052578b5ee165248bc54e5a511aa27b35e9d5
pnorton <peter.norton@gazprom-mt.com>
2020-01-09 15:44:18 +0000

    add hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,7 @@
-import os
 import sys
 from pprint import pprint as pp
 from pytube import YouTube
 
-print(os.getcwd())
 pp(sys.path)
 
 url = r'https://www.youtube.com/watch?v=xZAfdgYnsZc&list=PLYf3-nc0C5ly-2-7WU6S2nCW7MMT0VM87&index=2'
